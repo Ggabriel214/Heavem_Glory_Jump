c1841197178ca4422867a5129d3e21a08a872425
Ggabriel214 <90776092+Ggabriel214@users.noreply.github.com>
2022-05-21 14:03:10 +0800

    Ball selection and Ball skins

---
+++ Heaven Glory Jump/Assets/Scripts/Ball Selection/BallSelection.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BallSelection : MonoBehaviour
+{
+    [SerializeField] private Button previousButton;
+    [SerializeField] private Button nextButton;
+    private int currentCar;
+
+    private void Awake()
+    {
+        SelectBall(0);
+    }
+
+    private void SelectBall(int _index)
+    {
+        previousButton.interactable = (_index != 0);
+        nextButton.interactable = (_index != transform.childCount-1);
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(i == _index);
+        }
+    }
+
+    public void ChangeCar(int _change)
+    {
+        currentCar += _change;
+        SelectBall(currentCar);
+    }
+}
